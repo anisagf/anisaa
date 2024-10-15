69baadbf8b58589675e1e1585f02c57a2968f609
anisagf <anisagreyf@gmail.com>
2024-10-16 00:06:05 +0700

    Initial commit

---
+++ application/views/dosen/tambah.php
@@ -0,0 +1,22 @@
+<div class="container">
+	<div class="row mt-5">
+		<div class="col-md-7">
+		<div class="card">
+            <div class="card-header">
+                Form Tambah Data Dosen
+            </div>
+            <div class="card-body">
+         <form action="" method="post">
+            <div class="form-group">
+                <label for="nip">NIP</label>
+                <input type="text" class= "form-control" id="nip" name="nip">
+            </div>
+            <div class="form-group">
+                <label for="namadosen">Nama Dosen</label>
+                <input type="text" class= "form-control" id="namadosen" name="namadosen">
+            </div>
+            <button type="submit" name="tambah" class="btn-primary float-end">Tambah</button>
+            </form>
+		</div>
+	</div>   
+</div>
